04c99c6477fcfc59f4c8fa17d6dd52fe7d6e0dc4
aminulrony1999 <aminulrony1999@gmail.com>
2023-09-30 13:44:33 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -15,12 +15,25 @@ function ExternalUsers(){
 
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(user => setUsers(user))
+    .then(data => setUsers(data))
   }, []);
   return (
     <div>
       <h2>External Users</h2>
       <p>{users.length}</p>
+      {
+        users.map(user => <User name = {user.name} email={user.email} city={user.address.city}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div style ={{backgroundColor: 'lightpink',padding : '15px', border : '3px solid maroon', margin : '15px',borderRadius: '200px'}}>
+      <h3>Name : {props.name}</h3>
+      <h4>Email : {props.email}</h4>
+      <h5>City : {props.city}</h5>
     </div>
   )
 }
